337161f6a7e143e497984fc04f2feb5a09218629
c-bata <contact@c-bata.link>
2015-06-14 15:23:21 +0900

    connpassの勉強会の情報を表示

--- study_meeting.py
+++ study_meeting.py
@@ -1,5 +1,7 @@
 import requests
 
 json_file = requests.get('http://connpass.com/api/v1/event/?keyword=python')
-print(json_file.text)
+
+for item in json_file.json()['events']:
+    print(item['event_id'], item['title'])
 
