95b979151fa43b09c5fc08151213badf6ac9e210
c-bata <contact@c-bata.link>
2015-06-14 14:55:45 +0900

    5の倍数の時はBuzzと表示

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,5 +3,7 @@ if __name__ == '__main__':
     for i in range(100):
         if i % 3 == 0:
             print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
         else:
             print(i)
