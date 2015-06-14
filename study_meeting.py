84e4bc84865bff6bc4760901759447f821db0dcb
aoichi <sosososososochan@gmail.com>
2015-06-14 15:33:58 +0900

    好きなキーワードで検索できるように変更

--- study_meeting.py
+++ study_meeting.py
@@ -1,6 +1,9 @@
 import requests
 
-json_file = requests.get('http://connpass.com/api/v1/event/?keyword=python')
+keyword = input()
+url = 'http://connpass.com/api/v1/event/?keyword='
+
+json_file = requests.get(url + keyword)
 
 for item in json_file.json()['events']:
     print(item['event_id'], item['title'])
